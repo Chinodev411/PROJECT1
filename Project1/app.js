1b9159375c524c8dd505fb4e7b36499e11f03809
Chinonso Ogbennia <75351523+Chinodev411@users.noreply.github.com>
2021-06-09 12:09:29 -0400

    Add files via upload

--- Project1/app.js
+++ Project1/app.js
@@ -1,15 +1,35 @@
 //CACHED DOM ELEMENTS
 
-const card = document.querySelector ("#example-card");
+const card = document.querySelector("section");
+
+const flipCard = document.querySelector("#clickflip");
+
+const flipReveal = document.querySelector("#revealFlip");
+
+
+//FUNCS
+
+const onClickFlip = () => {
+  console.log(flipReveal);
+}
+
+flipCard.addEventListener("click", onClickFlip);
+
 
 
 
 //DEFINE FUNCTIONS
 
-const handleCardClicked = () => {
+const handleCardClicked=(e)=> {
   console.log("Hello from the clicked card");
+  e.target.parentElement.parentElement.classList.add("clicked");
 }
 
 // Add Event Listeners
 
-card.addEventListener ("click", handleCardClicked);
+card.addEventListener("click", handleCardClicked);
+
+
+
+
+
